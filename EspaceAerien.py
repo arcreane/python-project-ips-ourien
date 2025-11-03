424ea0f449a22dbe3455f2e76ebbd28550d84fac
Andrea Testa <andreatesta2017@gmail.com>
2025-11-03 16:54:55 +0100

    ajouts de fonctions dans EspaceAerien

--- EspaceAerien.py
+++ EspaceAerien.py
@@ -7,6 +7,12 @@ class EspaceAerien:
         self.avions.append(avion)
     def maj(self,dt):
         for avion in self.avions:
-            avion.deplacer(dt)
+            avion.move(dt)
     def detect_collisions(self,distance_min=1000):
         collisions = []
+        for i,a1 in enumerate(self.avions):
+            for a2 in self.avions[i+1:]:
+                dist=math.sqrt((a1.xa-a2.xa)**2 + (a1.ya-a2.ya)**2)
+                if dist < distance_min:
+                    collisions.append((a1,a2))
+        return collisions
