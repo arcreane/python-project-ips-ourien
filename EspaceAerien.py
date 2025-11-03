30e4788f64467b7726b73847c3b1395bbacdf656
Andrea Testa <andreatesta2017@gmail.com>
2025-11-03 16:38:35 +0100

    ajouts de fonctions dans EspaceAerien

--- EspaceAerien.py
+++ EspaceAerien.py
@@ -1,5 +1,12 @@
+from avion import Avion
+import math
 class EspaceAerien:
     def __init__(avions):
         self.avions = []
     def ajouter_avion(self, avion: Avion):
         self.avions.append(avion)
+    def maj(self,dt):
+        for avion in self.avions:
+            avion.deplacer(dt)
+    def detect_collisions(self,distance_min=1000):
+        collisions = []
