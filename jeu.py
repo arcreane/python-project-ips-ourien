8d603a03018f5192b0fdf179699ffed74186c149
Andrea Testa <andreatesta2017@gmail.com>
2025-11-03 17:18:41 +0100

    création et ajouts de fonctions dans le fichier jeu

--- jeu.py
+++ jeu.py
@@ -1,5 +1,4 @@
 from espace_aerien import EspaceAerien
-
 class Jeu:
     def __init__(self):
         self.espace = EspaceAerien()
@@ -9,4 +8,4 @@ class Jeu:
         self.espace.mettre_a_jour(dt)
         collisions = self.espace.detecter_collisions()
         if collisions:
-            print("Collision détectée !")
+            print("Collision détectée")
