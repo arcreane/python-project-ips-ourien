444ad2e74cdac185b51edaf54304ced2ebd41f15
Loic Lemesreqqquitgit config --global user.name Loic <loic.lemesre@ipsa.fr>
2025-11-12 14:46:07 +0100

    Création du bouton augmenter l'altitude

--- interface.py
+++ interface.py
@@ -8,17 +8,21 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-
+        button = QPushButton("Altitude +500 ft ")
         self.setWindowTitle("My App")
+        button.setCheckable(True)
+        button.clicked.connect(self.the_button_was_clicked)
 
-        button = QPushButton("Press Me!")
+
 
         self.setFixedSize(QSize(400, 300))
 
         # Set the central widget of the Window.
         self.setCentralWidget(button)
-
-
+    def the_button_was_clicked(self):
+            print("L'altitude de l'avion.identifiant a augmentée de 500ft")
+            if avion.altitude() >= 11000:
+                print("l'avion ne peut pas monter plus haut")
 app = QApplication(sys.argv)
 
 window = MainWindow()
