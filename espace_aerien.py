9af1a69336e3a2be317810a1d2311ac905eba20c
Andrea Testa <andreatesta2017@gmail.com>
2025-11-12 14:30:41 +0100

    Test

--- espace_aerien.py
+++ espace_aerien.py
@@ -1,18 +1,23 @@
-from avion import Avion
+from app import Avion
 import math
+
+
 class EspaceAerien:
-    def __init__(avions):
+    def __init__(self, avions):
         self.avions = []
+
     def ajouter_avion(self, avion: Avion):
         self.avions.append(avion)
-    def maj(self,dt):
+
+    def maj(self, dt):
         for avion in self.avions:
             avion.move(dt)
-    def detect_collisions(self,distance_min=1000):
+
+    def detect_collisions(self, distance_min=1000):
         collisions = []
-        for i,a1 in enumerate(self.avions):
-            for a2 in self.avions[i+1:]:
-                dist=math.sqrt((a1.xa-a2.xa)**2 + (a1.ya-a2.ya)**2)
+        for i, a1 in enumerate(self.avions):
+            for a2 in self.avions[i + 1:]:
+                dist = math.sqrt((a1.xa - a2.xa) ** 2 + (a1.ya - a2.ya) ** 2)
                 if dist < distance_min:
-                    collisions.append((a1,a2))
+                    collisions.append((a1, a2))
         return collisions
